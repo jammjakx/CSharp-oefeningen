a8e2b9eea028767412097280451fbd5821b1c4f1
Ralph V <Ralphverspui@live.nl>
2017-10-09 12:53:49 +0200

    Initial commit

---
+++ Classes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Classes.Math;
+
+namespace CSharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var john = new Person();
+            john.FirstName = "John";
+            john.LastName = "smith";
+            john.Introduce();
+
+            Calculator calculator = new Calculator();
+            var result = calculator.Add(1, 2);
+            Console.WriteLine(result);
+        }
+    }
+}
