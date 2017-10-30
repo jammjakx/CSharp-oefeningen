36800480b8aafb4733d95f72310b2d8457e4511f
jammjakx <ralphverspui@live.nl>
2017-10-30 13:55:47 +0100

    Add files via upload

---
+++ ForEach/forEach/forEach/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace forEach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[] {1, 2, 3, 4};
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
+
+
+
+//DELETED CODE
+//var name = "John Smith";
+
+////for (var i = 0; i < name.Length; i++)
+////{
+////    Console.WriteLine(name[i]);
+////}
+
+//foreach (var character in name)
+//{
+//Console.WriteLine(character);
+//}
