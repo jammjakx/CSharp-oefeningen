27e782c1761fd56d99e20aed1d8cac96386dd863
jammjakx <ralphverspui@live.nl>
2017-10-30 14:00:18 +0100

    Add files via upload

---
+++ exercise1/exercise1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number between 1 and 10, and press Enter.");
+            var input = Console.ReadLine();
+            var number = Convert.ToInt32(input);
+
+            if (number >= 1 && number <= 10)
+                Console.WriteLine("Valid");
+
+            else
+                Console.WriteLine("Invalid");
+        }
+    }
+}
