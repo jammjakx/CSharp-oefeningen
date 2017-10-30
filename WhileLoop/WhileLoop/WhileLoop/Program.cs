d36743c3b8ee237b6232f8e901ec90005e3dbd9f
jammjakx <ralphverspui@live.nl>
2017-10-30 13:40:52 +0100

    whileLoop

---
+++ WhileLoop/WhileLoop/WhileLoop/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Type your name: ");
+                var input = Console.ReadLine();
+
+                if (!string.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("@Echo: " + input);
+                    continue;
+                }
+
+                break;
+            }
+        }
+    }
+}
+
+
+
+//DELETED CODE
+//for (var i = 1; i <= 10; i++)
+//{
+//    if (i%2 == 0)
+//        Console.WriteLine(i);
+//}
+
+//var i = 0;
+//while (i % 2 == 0)
+//{
+//    if ( i % 2 == 0)
+//    Console.WriteLine(i);
+
+//    i++;
+//}
