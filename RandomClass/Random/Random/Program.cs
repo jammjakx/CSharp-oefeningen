f6bc18f42c31611a158839347f060d437969d1c2
jammjakx <ralphverspui@live.nl>
2017-10-31 09:50:42 +0100

    Add files via upload

---
+++ RandomClass/Random/Random/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Random
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new System.Random();
+
+            const int passwordLength = 10;
+            var buffer = new char[passwordLength];
+
+            for (var i = 0; i < passwordLength; i++)
+                buffer[i] = ((char)('a' + random.Next(0, 26)));
+
+            var password = new string(buffer);
+
+            Console.WriteLine(password);
+        }
+    }
+}
